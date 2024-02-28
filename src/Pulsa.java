dde09acd42e324697934825cf0b7369a3f93efe4
IlmanNafi11 <studyilmannafi@gmail.com>
2024-02-28 12:14:04 +0700

    koding kelas Pulsa

--- src/Pulsa.java
+++ src/Pulsa.java
@@ -1,8 +1,15 @@
 public class Pulsa extends Product{
 
-    private String kategori;
-    public Pulsa(String idProduct, String namaProduct, int hargaProduct, int jumlahStock, String kategori) {
+    private String founder;
+    public Pulsa(String idProduct, String namaProduct, int hargaProduct, int jumlahStock, String kategori, String founder) {
         super(idProduct, namaProduct, hargaProduct, jumlahStock, kategori);
+        this.founder = founder;
+    }
+
+    @Override
+    public void displayDetail(){
+        super.displayDetail();
+        System.out.println("Founder : " + founder);
     }
 
 }
