04e30eaa9edbe4ada0010e5e920135430f4272eb
IlmanNafi11 <studyilmannafi@gmail.com>
2024-02-28 13:04:52 +0700

    fix kelas aksesoris

--- src/Aksesoris.java
+++ src/Aksesoris.java
@@ -1,20 +1,11 @@
 public class Aksesoris extends Product {
     private String tipe;
-    private int jumlahPembelian;
 
     public Aksesoris(String idProduct, String namaProduct, int hargaProduct, int jumlahStock, String kategori,
             String tipe) {
         super(idProduct, namaProduct, hargaProduct, jumlahStock, kategori);
         this.tipe = tipe;
     }
-
-    public Aksesoris(String idProduct, String namaProduct, int hargaProduct, int jumlahStock, String kategori,
-            String tipe, int jumlahPembelian) {
-        super(idProduct, namaProduct, hargaProduct, jumlahStock, kategori);
-        this.tipe = tipe;
-        this.jumlahPembelian = jumlahPembelian;
-    }
-
     @Override
     public void displayDetail() {
         super.displayDetail();
