d34fde938eebcfab3851ec9ae76c7791f6d1a120
IlmanNafi11 <studyilmannafi@gmail.com>
2024-02-28 11:57:09 +0700

    coding kelas Aksesoris

--- src/Aksesoris.java
+++ src/Aksesoris.java
@@ -1,7 +1,14 @@
 public class Aksesoris extends Product{
+    private String kategori;
 
-    public Aksesoris(String idProduct, String namaProduct, int hargaProduct, int jumlahStock) {
+    public Aksesoris(String idProduct, String namaProduct, int hargaProduct, int jumlahStock, String kategori) {
         super(idProduct, namaProduct, hargaProduct, jumlahStock);
+        this.kategori = kategori;
+    }
+    @Override
+    public void displayDetail(){
+        super.displayDetail();
+        System.out.println("Kategori : " + kategori);
     }
     
 }
