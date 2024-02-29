88fa0492a16e846ffc7b0850691e79ea7592e0d1
IlmanNafi11 <studyilmannafi@gmail.com>
2024-03-01 06:08:23 +0700

    koding implementasi di kelas main

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Aksesoris aksesoris = new Aksesoris("AKS-001", "Casing", 15000, 5, "Aksesoris", "Redmi 9C");
-        // aksesoris.displayDetail();
-        // System.out.println();
-        // Elektronik elektronik = new Elektronik("ELK-001", "Samsung Tab 10", 1000000, 2, "Elektronik", "Tablet");
-        // elektronik.displayDetail();
-        // System.out.println();
-        // Pulsa pulsa = new Pulsa("PLS-001", "Paket Data 4GB", 35000, 5, "Paket Data", "Telkomsel");
-        // pulsa.displayDetail();
-        Product product = new Product("PROD-1", "Casing", 50000, 2, "Suku cadang");
-        // Transaksi transaksi = new Transaksi("Tran-01", product);
-        // transaksi.displayDetail();
-        Transaksi tr = new Transaksi("TR-001", product, 4, 230000);
-        TransaksiPenjualan transaksiPenjualan = new TransaksiPenjualan("TP-001", product, 2, 120000, "08539326885");
-        Service service = new Service("SER-001", product, 1, 40000, "Ilman", "0852986", "Sragen", "Ganti Casing");
+        // buat object scanner
+        Scanner sc = new Scanner(System.in);
+
+        // konsep polymorphism
+        Product aksesoris = new Aksesoris("AKS-001", "Casing", 20000, 2, "Pelindung Layar", "5.3 Inc");
+        ((Product)aksesoris).displayDetail();
+        // ((Product) aksesoris).displayDetail();;
+        System.out.println("================================");
+        
+        System.out.println("======== Transaksi ========");
+        // meminta inputan ke user
+        System.out.print("Masukan jumlah bayar service : Rp. ");
+        int jumlahBayar = sc.nextInt();
+
+        // menampilkan detail transaksi service
+        Service service = new Service("SER-001", aksesoris, 1, jumlahBayar, "Ilman", "0852986", "Sragen", "Ganti Casing");
         service.hitungTotal();
         service.hitungRefund();
         service.displayDetail();
+
+        System.out.println();
+        System.out.println("======= Transaksi =======");
+        // meminta inputan ke user
+        System.out.print("Masukan jumlah bayar Pembelian : Rp. ");
+        jumlahBayar = sc.nextInt();
+
+        // menampilkan detail transaksi penjualan
+        TransaksiPenjualan transaksiPenjualan = new TransaksiPenjualan("TP-002", aksesoris, 2 , jumlahBayar, "087675656");
+        transaksiPenjualan.hitungTotal();
+        transaksiPenjualan.hitungRefund();
+        transaksiPenjualan.displayDetail();
     }
 }
